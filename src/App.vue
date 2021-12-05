b393cc603e5e8b7424803653c65e542067326243
takumikunn15 <62226679+takumikunn15@users.noreply.github.com>
2021-12-05 14:58:16 +0900

    first commit

---
+++ src/App.vue
@@ -0,0 +1,130 @@
+<script setup>
+import CountDownWrapper from './components/CountDownWrapper.vue';
+</script>
+
+<template>
+  <div id="bg-video" class="w-screen h-screen fixed filter blur-md">
+    <video
+      class="w-full h-full object-cover"
+      src="./assets/okayun-birthday-countdown-back-video.mp4"
+      poster="./assets/okayun-birthday-countdown-back-video_poster.webp"
+      autoplay
+      muted
+      playsinline
+      loop
+    ></video>
+  </div>
+  <CountDownWrapper></CountDownWrapper>
+  <div id="page-title-wrapper" class="display-width">
+    <p id="page-title" class="text-sm lg:text-base text-center text-white">おかゆんお誕生日カウントダウン(非公式)</p>
+  </div>
+  <div id="copy-wright-wrapper" class="display-width">
+    <a
+      href="https://twitter.com/niwatoriazi"
+      target="_blank"
+      id="copy-right"
+      class="text-xs lg:text-base text-center underline text-white"
+    >©2021 にわとりあじ</a>
+  </div>
+  <div id="frame" class="fixed inset-0 w-screen h-screen pointer-events-none"></div>
+  <a
+    href="https://niwatoriazi.github.io/korosan-birthday-countdown/"
+    id="twitter-share-link"
+    class="fixed right-10 bottom-38 lg:right-20 lg:bottom-76 transform hover:-rotate-6 hover:scale-110 duration-200"
+  >
+    <img
+      src="./assets/1f950.svg"
+      class="w-24"
+    />
+  </a>
+  <a
+    href="https://twitter.com/nekomataokayu"
+    id="twitter-share-link"
+    target="_blank"
+    class="fixed right-10 bottom-24 lg:right-20 lg:bottom-48 transform hover:-rotate-6 hover:scale-110 duration-200"
+  >
+    <img class="w-16 lg:w-24" src="./assets/2021Twitter_logo-blue.png" alt="twitter-logo" />
+  </a>
+  <a
+    href="https://youtube.com/channel/UChAnqc_AY5_I3Px5dig3X1Q"
+    id="youtube-channel-link"
+    target="_blank"
+    class="fixed right-10 bottom-10 lg:right-20 lg:bottom-20 transform hover:-rotate-6 hover:scale-110 duration-200"
+  >
+    <img class="w-16 lg:w-24" src="./assets/youtube_social_icon_red.webp" alt="youtube-logo" />
+  </a>
+</template>
+
+<style lang="scss">
+@import url(https://fonts.googleapis.com/css2?family=DotGothic16&family=Hachi+Maru+Pop&family=Press+Start+2P&display=swap);
+@supports (-webkit-touch-callout: none) {
+  #bg-video {
+    height: -webkit-fill-available;
+  }
+  #frame {
+    height: -webkit-fill-available;
+  }
+  #count-down-wrapper {
+    height: -webkit-fill-available;
+    overflow: hidden;
+  }
+}
+body {
+  background-color: #b190fc;
+  cursor: url(./assets/onigiri.png), auto;
+}
+#party {
+  z-index: -1;
+  cursor: url(./assets/onigiri.png), auto;
+}
+.z-layer:not(:first-child) {
+  color: #455a64;
+}
+#page-title-wrapper {
+  z-index: 1;
+  width: 100vw;
+  height: 50px;
+  @media (orientation: portrait) {
+    height: 30px;
+  }
+  top: 0px;
+  position: fixed;
+  display: grid;
+  place-content: center;
+  #page-title {
+    font-family: "Hachi Maru Pop", cursive;
+  }
+}
+#copy-wright-wrapper {
+  z-index: 1;
+  width: 100vw;
+  height: 50px;
+  @media (orientation: portrait) {
+    height: 30px;
+  }
+  bottom: 0px;
+  position: fixed;
+  display: grid;
+  place-content: center;
+  #copy-right {
+    cursor: url(./assets/negitoro-onigiri.png), pointer;
+    font-family: "Hachi Maru Pop", cursive;
+  }
+}
+#bg-video {
+  z-index: -3;
+}
+#frame {
+  border: solid 50px #b190fc;
+  z-index: 0;
+  @media (orientation: portrait) {
+    border: solid 30px #b190fc;
+  }
+}
+#twitter-share-link {
+  cursor: url(./assets/negitoro-onigiri.png), pointer;
+}
+#youtube-channel-link {
+  cursor: url(./assets/negitoro-onigiri.png), pointer;
+}
+</style>
---
+++ src/components/CountDownWrapper.vue
@@ -0,0 +1,67 @@
+﻿<script setup>
+import { ref, computed } from 'vue'
+import dayjs from 'dayjs'
+
+// define all timer variables
+const day = ref(0);
+const hour = ref(0);
+const minute = ref(0);
+const second = ref(0);
+
+setInterval(function () {
+    const distance = dayjs('2022-02-22 00:00:00').diff(dayjs())
+    day.value = Math.floor(distance / (1000 * 60 * 60 * 24));
+    hour.value = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    minute.value = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    second.value = Math.floor((distance % (1000 * 60)) / 1000);
+}, 100);
+
+const getDay = computed(() => {
+    let digit = 3;
+    return (Array(digit).join('0') + day.value).slice(-digit);
+});
+const getHour = computed(() => {
+    let digit = 2;
+    return (Array(digit).join('0') + hour.value).slice(-digit);
+});
+const getMinute = computed(() => {
+    let digit = 2;
+    return (Array(digit).join('0') + minute.value).slice(-digit);
+});
+const getSecond = computed(() => {
+    let digit = 2;
+    return (Array(digit).join('0') + second.value).slice(-digit);
+});
+</script>
+<template>
+    <div
+        id="count-down-wrapper"
+        class="w-screen h-screen grid place-items-center pointer-events-none fixed"
+    >
+        <h1
+            id="count-down-timer"
+            class="lg:text-9xl text-6xl lg:text-center md:text-left leading-tight lg:leading-snug whitespace-normal whitespace-no-wrap"
+        >
+            <span class="lg:text-7xl text-3xl">誕生日まであと</span><br>{{getDay}}<span class="lg:text-7xl text-3xl">日</span><br class="lg:hidden">{{getHour}}<span class="lg:text-7xl text-3xl">時間</span><br />{{getMinute}}<span class="lg:text-7xl text-3xl">分</span><br class="lg:hidden">{{getSecond}}<span class="lg:text-7xl text-3xl">秒</span>
+        </h1>
+    </div>
+</template>
+<style lang="scss">
+#count-down-wrapper {
+    z-index: -2;
+    #count-down-timer {
+        font-family: "Press Start 2P", "DotGothic16", cursive;
+        color: white;
+        filter: drop-shadow(0px 0px 2.2px rgba(0, 0, 0, 0.042))
+            drop-shadow(0px 0px 5.3px rgba(0, 0, 0, 0.061))
+            drop-shadow(0px 0px 10px rgba(0, 0, 0, 0.075))
+            drop-shadow(0px 0px 17.9px rgba(0, 0, 0, 0.089))
+            drop-shadow(0px 0px 33.4px rgba(0, 0, 0, 0.108))
+            drop-shadow(0px 0px 80px rgba(0, 0, 0, 0.15));
+
+        span {
+            vertical-align: 25%;
+        }
+    }
+}
+</style>
